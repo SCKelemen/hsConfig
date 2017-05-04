c87037ec197386e52605f59db23aea8754ad393c
Samuel Kelemen <SCKelemen@users.noreply.github.com>
2017-05-04 02:16:35 -0500

    Update config.go

--- config/config.go
+++ config/config.go
@@ -2,5 +2,27 @@ package config
 
 import (
   "fmt"
+  "time"
   "github.com/SCKelemen/hsConfig/errors"
 )
+
+type Properties map[string]interface{}
+
+func (p Properties) Validate() error {
+}
+
+func (p Properties) Get(key string)interface{} {
+  if p == nil {
+    return nil
+  }
+  return p[key]
+}
+
+func (p Properties) Set(key string, val interface{}) {
+  p[key] = val
+}
+
+func (p Properties) Rem(key string) {
+  delete(p, key)
+}
+
